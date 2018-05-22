9c6b69710beb58e078247f566297f403eda843bf
zoofa <zoofa@ymail.com>
2018-05-22 20:29:37 +0430

    Refactor of method FizzBuzz for passing Div5Test

--- FizzBuzz/FizzBuzz.Library/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzz.Library/FizzBuzzer.cs
@@ -6,6 +6,11 @@ namespace FizzBuzz.Library
     {
         public static string FizzBuzz(int input)
         {
+            if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+
             if (input % 3 == 0)
             {
                 return "Fizz";
