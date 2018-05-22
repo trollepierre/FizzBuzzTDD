fe248675440481e63f6698746d913b58c6726bcf
zoofa <zoofa@ymail.com>
2018-05-22 20:26:02 +0430

    Refactor of method FizzBuzz for passing Div3Test

--- FizzBuzz/FizzBuzz.Library/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzz.Library/FizzBuzzer.cs
@@ -6,6 +6,10 @@ namespace FizzBuzz.Library
     {
         public static string FizzBuzz(int input)
         {
+            if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
             return input.ToString();
         }
     }
